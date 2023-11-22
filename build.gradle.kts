729047202f925a4a3b300a27b8ec042bb2bbcc73
dawoud <ashrafdawoud1111@gmail.com>
2023-11-22 20:04:00 +0200

    first

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    //trick: for the same plugin versions in all sub-modules
+    id("com.android.library").version("8.0.0").apply(false)
+    kotlin("multiplatform").version("1.8.21").apply(false)
+    id("maven-publish")
+}
+group = "com.example"
+version = "1.0"
+
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/ashrafdawoud/NotifyPro")
+            credentials {
+                username = "ashrafdawoud"
+                password = "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"
+            }
+        }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
